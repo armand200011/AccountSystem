9cc157bd3daffa50dc497b0f5c7dccd5ce73da77
armand200011 <60873120+armand200011@users.noreply.github.com>
2021-09-03 16:34:07 +0200

    Created SpringBoot(AccountSystemWebSpringBoot) Busy with SpringBoot context Database entities still must be created

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -0,0 +1,14 @@
+package za.ac.nwu.ac.web.sb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import za.ac.nwu.ac.domain.service.GeneralResponse;
+
+@RestController
+public class AccountTypeController {
+
+    @GetMapping("/all")
+    public GeneralResponse<String> getAll(){
+        return new GeneralResponse<String>(true, "No types found.");
+    }
+}
