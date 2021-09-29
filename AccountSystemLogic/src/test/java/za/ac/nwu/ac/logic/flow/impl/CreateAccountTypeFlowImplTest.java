f90d3766526ba9e2d1572c44afbd4076f0102936
armand200011 <60873120+armand200011@users.noreply.github.com>
2021-09-29 11:29:24 +0200

    Implemented all the different ways of unit testing in the in-memory db with the exception of the transaction table because still have to add the code to link them

---
+++ AccountSystemLogic/src/test/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeFlowImplTest.java
@@ -0,0 +1,48 @@
+package za.ac.nwu.ac.logic.flow.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
+import za.ac.nwu.ac.translator.AccountTypeTranslator;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CreateAccountTypeFlowImplTest {
+
+    @Mock
+    private AccountTypeTranslator translator;
+    @InjectMocks
+    private CreateAccountTypeFlowImpl flow;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void create() {
+        try {
+            AccountTypeDto accountTypeDto = new AccountTypeDto("accountTypeCode", "name", LocalDate.parse("2021-09-29"));
+            when(translator.create(any(AccountTypeDto.class))).thenReturn(accountTypeDto);
+            AccountTypeDto res = flow.create(new AccountTypeDto());
+            assertNotNull(res);
+            verify(translator, atLeastOnce()).create(any(AccountTypeDto.class));
+        } catch (Exception e) {
+            assertTrue("Error message not as expected", e.getMessage().equalsIgnoreCase("Some reasone the CreateAccountTypeFlowImplTest could not complete"));
+        }
+    }
+}
---
+++ AccountSystemRepository/src/test/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepositoryTest.java
@@ -0,0 +1,38 @@
+package za.ac.nwu.ac.repo.persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import za.ac.nwu.ac.domain.persistence.AccountType;
+import za.ac.nwu.ac.repo.config.RepositoryTestConfig;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ContextConfiguration(classes= {RepositoryTestConfig.class})
+public class AccountTypeRepositoryTest {
+
+    @Autowired
+    AccountTypeRepository accountTypeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAccountTypeByAccountTypeCodeNativeQuery() {
+        AccountType miles = accountTypeRepository.getAccountTypeByAccountTypeCodeNativeQuery("MILESSS");
+        assertNull(miles);
+        //assertEquals("MILES", miles.getAccountTypeCode());
+    }
+}
